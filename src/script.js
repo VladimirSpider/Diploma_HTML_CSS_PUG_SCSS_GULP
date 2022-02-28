5ae40d64f5f78149357ec3d1fabd82573d48c6f0
Vladimir <vovchikbura@mail.ru>
2022-02-28 23:58:21 +0300

    feat: burger menu js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,17 @@
-console.log('hi')
+const closeBurgerMenu = document.querySelector('.close_burger_menu');
+const closeBurger = document.querySelector('.close_burger');
+const openBurger = document.querySelector('.open_burger_menu')
+const buttonCloseBurger = document.querySelector('.button_close_burger_menu a')
+
+const callBack = () => {
+	closeBurger.classList.toggle("close_burger_hidden");
+	openBurger.classList.toggle("open_burger_menu_show");
+}
+
+closeBurgerMenu.addEventListener('click', () => {
+	callBack();
+})
+
+buttonCloseBurger.addEventListener('click', () => {
+	callBack();
+})
